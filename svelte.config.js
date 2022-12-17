aa97df07ae46a8e6379283e5fa4cdcf95a4795f9
somni <me@somni.one>
2022-12-17 23:19:53 +0900

    Add `./src` path alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from "path";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
@@ -9,6 +10,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		alias: {
+			"@": path.resolve("./src"),
+		},
 	},
 };
 
