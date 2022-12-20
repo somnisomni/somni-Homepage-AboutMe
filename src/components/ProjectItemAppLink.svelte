5fd781533d2ac95b3cbea3221e0a9ac6434aafa4
somni <me@somni.one>
2022-12-20 21:08:25 +0900

    Add `<a>` tag hover style

--- src/components/ProjectItemAppLink.svelte
+++ src/components/ProjectItemAppLink.svelte
@@ -1,6 +1,6 @@
 <!-- App link -->
 <div class="text-right font-semibold text-sky-600 mt-2">
-  <a href={ url } class="p-2 text-lg after:content-['→'] after:ml-2">{ text }</a>
+  <a href={ url } class="no-underline p-2 text-lg after:content-['→'] after:ml-2">{ text }</a>
 </div>
 
 <script lang="ts">
