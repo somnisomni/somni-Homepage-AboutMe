220dd0d86aee883955cf4473ace510e2a157d4bd
somni <me@somni.one>
2023-01-25 00:10:58 +0900

    Show text while loading initial resources

--- src/components/landing/SceneLoading.svelte
+++ src/components/landing/SceneLoading.svelte
@@ -1,10 +1,12 @@
 <div id="scene-boot-up"
      class="flex flex-col absolute left-0 top-0 right-0 bottom-0 z-50 justify-center items-center w-screen h-screen">
-  <span class="anim2 font-thin text-5xl sm:text-6xl mb-16">Loading…</span>
+  <span class="anim2 font-thin text-5xl sm:text-6xl">Loading…</span>
 
-  <div class="anim1">
+  <div class="anim1 mt-16">
     <LoadingSpinner />
   </div>
+
+  <div class="anim3 font-thin text-sm text-center fixed left-0 right-0 bottom-8 w-screen">loading essential resources…</div>
 </div>
 
 <script lang="ts">
@@ -23,12 +25,22 @@ $: {
       opacity: 0,
       duration: 1000,
       easing: "easeOutExpo",
-      delay: anime.stagger(200),
+      delay: anime.stagger(200, { direction: "reverse" }),
     }).finished.then(() => { exitCompleted = true; });
   }
-};
+}
 
 onMount(() => {
+  anime({
+    targets: "#scene-boot-up .anim3",
+    opacity: 0,
+    duration: 250,
+    easing: "linear",
+    complete() {
+      document.querySelector("#scene-boot-up .anim3")?.remove();
+    },
+  });
+
   anime({
     targets: "#scene-boot-up .anim1",
     translateZ: "-2em",
