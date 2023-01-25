2004497ba8fb5b0fab3bb928f7dd10157bf448cb
somni <me@somni.one>
2023-01-25 20:43:39 +0900

    Pick wallpaper image from hand-picked ID list

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+const picsumIds = [537];
+
 export const LOCALSTORAGE_NAMESPACE = "somni.homepage";
 export const LOCALSTORAGE_PROP_VISITED = `${LOCALSTORAGE_NAMESPACE}.visited`;
-export const SEED = new Date().getMilliseconds();
+export const WALLPAPER_PICSUM_ID = picsumIds[Math.floor(Math.random() * picsumIds.length)];
