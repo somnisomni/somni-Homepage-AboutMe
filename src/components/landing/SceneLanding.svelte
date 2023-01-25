b30cb39ee2478e1e0b3042c9d06b6bbaf1b24db2
somni <me@somni.one>
2023-01-25 20:43:57 +0900

    Create bare link icon component

--- src/components/landing/SceneLanding.svelte
+++ src/components/landing/SceneLanding.svelte
@@ -1,8 +1,10 @@
 <div id="scene-landing">
   <Wallpaper /> <!-- #bg -->
   
-  <div class="z-10">
-
+  <div id="front" class="z-10">
+    <div id="link-icons">
+      <LinkIcon />
+    </div>
   </div>
 </div>
 
@@ -10,15 +12,20 @@
 import { onMount } from "svelte";
 import anime from "animejs";
 import Wallpaper from "./components/Wallpaper.svelte";
+import LinkIcon from "./components/LinkIcon.svelte";
 
 onMount(() => {
-  anime({
+  anime.timeline().add({
     targets: "#scene-landing #bg",
-    scale: 0.85,
-    opacity: 0,
-    direction: "reverse",
-    duration: 1500,
-    easing: "easeInQuint",
+    scale: [0.9, 1],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: "easeOutQuint",
+  }).add({
+    targets: "#scene-landing #front",
+    opacity: [0, 1],
+    duration: 1000,
+    easing: "easeOutQuint",
   });
 });
 </script>
---
+++ src/components/landing/components/LinkIcon.svelte
@@ -0,0 +1,7 @@
+<div class="link-icon">
+  ICON
+</div>
+
+<script lang="ts">
+
+</script>
